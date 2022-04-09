a366d0b133e5f38f8c1457747b06e6dca9fbc312
seohyun-106 <changseohyun@sookmyung.ac.kr>
2022-04-09 11:36:27 +0900

    add: 1주차 실천과제

---
+++ SOPT-server-30/Seminar/seminar1/array.js
@@ -0,0 +1,5 @@
+// let arr = [1, 'item', true];
+// let arr2 = Array(4, null, {item: 'item'});
+
+// arr.map(item => console.log(item));
+// arr2.map(item => console.log(item));
---
+++ SOPT-server-30/Seminar/seminar1/type.ts
@@ -0,0 +1,10 @@
+let isDone: boolean = true;
+const str: string = 'hello';
+let num: number = 2;
+
+let arr: number[] = [1,2,3];
+const strArr: Array<string> = ['hello', 'world'];
+const objArr: Array<object> = [
+  {item: 'value'},
+  {item: 'value2'}
+];
